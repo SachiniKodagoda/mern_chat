2c87b4be6e76e4928bdf7e271faccd9ebfefe655
SachiniKodagoda <sachikodagoda96@gmail.com>
2023-07-26 01:46:23 +0530

    create register form

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Register(){
+    const[username,setUsername] = useState('');
+    const[password,setPassword] = useState('');
     return(
-        <div className="h-screen bg-blue-50">
-            <form className="w-64 mx-auto">
-                <input type="text" placeholder="Username" className="block w-full p-2 mb-2 rounded-sm"/>
-                <input type="password" placeholder="Password" className="block w-full p-2 mb-2 rounded-sm"/>
-                <button className="block w-full text-white bg-blue-500 rounded-sm">Register</button>
+        <div className="flex items-center h-screen bg-blue-50">
+            <form className="w-64 mx-auto mb-12">
+                <input 
+                    value={username} 
+                    onChange={ev=> setUsername(ev.target.value)} 
+                    type="text" 
+                    placeholder="Username" 
+                    className="block w-full p-2 mb-2 border rounded-sm"/>
+                <input 
+                    value={password} 
+                    onChange={ev=> setPassword(ev.target.value)} 
+                    type="password" 
+                    placeholder="Password" 
+                    className="block w-full p-2 mb-2 border rounded-sm"/>
+                <button className="block w-full p-2 text-white bg-blue-500 rounded-sm">Register</button>
             </form>
         </div>
     );
